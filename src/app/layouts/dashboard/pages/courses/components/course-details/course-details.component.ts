aa5b0d81c8eea7bcf8d6e245f9fcc644cbbdc48d
bret <38740892+betcheco@users.noreply.github.com>
2024-02-12 18:37:17 +0100

    implented getid in details course

--- src/app/layouts/dashboard/pages/courses/components/course-details/course-details.component.ts
+++ src/app/layouts/dashboard/pages/courses/components/course-details/course-details.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-course-details',
   templateUrl: './course-details.component.html',
   styleUrl: './course-details.component.scss'
 })
-export class CourseDetailsComponent {
+export class CourseDetailsComponent implements OnInit {
+  courseId : string | null = '0';
+  constructor(private router:Router,
+    private route: ActivatedRoute){}
+
+  ngOnInit(): void {
+    this.courseId = this.route.snapshot.paramMap.get('id');
+  }
+
 
 }
